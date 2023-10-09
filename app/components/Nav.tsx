c0c5181705bb277305541d8431fe1141ed5ba780
Damian Puś <pusiek99@interia.pl>
2023-10-09 10:00:09 +0200

    Created nav

---
+++ app/components/Nav.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React from 'react';
+import type { Session } from 'next-auth';
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+
+const Nav = ({ user }: Session) => {
+  console.log(user);
+
+  return (
+    <nav className="flex items-center justify-between py-8">
+      <h1>Nav</h1>
+      <ul className="flex items-center gap-12">
+        {!user && (
+          <li className="rounded-md bg-teal-600 p-2 text-white">
+            <button onClick={() => signIn()}>Sign in</button>
+          </li>
+        )}
+        {user && (
+          <li>
+            <Image
+              src={user?.image as string}
+              alt={user.name as string}
+              width={48}
+              height={48}
+              className="rounded-full"
+            />
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
