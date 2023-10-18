7b046c5539fe86899c244376e77a016b7d662caa
Damian Puś <pusiek99@interia.pl>
2023-10-18 21:27:47 +0200

    Stripe webhooks - paymentIntent and chargeSucceeded

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,9 +1,6 @@
 import Stripe from 'stripe';
 import client from '@/libs/prisma';
-import { buffer } from 'micro';
-import { NextRequest } from 'next/server';
 import { headers } from 'next/headers';
-import { NextApiRequest } from 'next';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2023-08-16',
@@ -11,11 +8,44 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
 
 export async function POST(request: Request) {
   // const buf = await buffer(request as NextApiRequest);
-  const signature = headers().get('stripe-signature');
-}
+  const signature = headers().get('stripe-signature') as string;
+  const body = await request.text();
 
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+  if (!signature)
+    return Response.json('Missing the stripe signature', {
+      status: 400,
+    });
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!,
+    );
+  } catch (e: any) {
+    return Response.json('Webhook Error' + e.message);
+  }
+
+  switch (event.type) {
+    case 'payment_intent.created':
+      const paymentIntent = event.data.object;
+      console.log('Payment intent was created');
+      break;
+
+    case 'charge.succeeded':
+      const charge = event.data.object as Stripe.Charge;
+      if (typeof charge.payment_intent === 'string') {
+        const order = await client.order.update({
+          where: { paymentIntentID: charge.payment_intent },
+          data: { status: 'complete' },
+        });
+      }
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return Response.json({ received: true });
+}
